2698902a4d4ec8603b338998dd7be41c4d94ac7d
Malhar <malhardevasthali007@gmail.com>
2026-03-24 22:10:09 +0530

    Added TeamCard View Prefab and Fetched The Data

--- Assets/Scripts/UI/TeamCardView.cs
+++ Assets/Scripts/UI/TeamCardView.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -5,13 +6,24 @@ using UnityEngine.UI;
 public class TeamCardView : MonoBehaviour
 {
     public PlayerData data;
+
+    [Header("UI Elements")]
     [SerializeField] private Sprite emptySlotSprite;
-    
+    [SerializeField] private TextMeshProUGUI battingPowerText;
+    [SerializeField] private TextMeshProUGUI bowlingPowerText;
+    [SerializeField] private TextMeshProUGUI defenceText;
+    [SerializeField] private RectTransform BattingPowerUI;
+    [SerializeField] private RectTransform BowlingPowerUI;
+    [SerializeField] private RectTransform DefenceUI;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioClip buttonClickSound;
+
+
     private Button button;
     private TeamSelectionService teamSelectionService;
 
-    [SerializeField] private AudioClip buttonClickSound;
-
+ 
     void Awake()
     {
         button = GetComponent<Button>();
@@ -25,10 +37,35 @@ public class TeamCardView : MonoBehaviour
     {
         this.data = data;
         GetComponent<Image>().sprite = data.cardSprite;
+
+        LoadUIForCard(data);
+
     }
+
+    private void LoadUIForCard(PlayerData data)
+    {
+        battingPowerText.text = data.BattingPower.ToString();
+        bowlingPowerText.text = data.BowlingPower.ToString();
+        defenceText.text = data.Defense.ToString();
+        DefenceUI.gameObject.SetActive(true);
+        if (data.role == PlayerRole.Batsman)
+        {
+            BattingPowerUI.gameObject.SetActive(true);
+            BowlingPowerUI.gameObject.SetActive(false);
+        }
+        else
+        {
+            BattingPowerUI.gameObject.SetActive(false);
+            BowlingPowerUI.gameObject.SetActive(true);
+        }
+    }
+
     public void RemoveFromTeam()
     {
         GetComponent<Image>().sprite = emptySlotSprite;
+        BattingPowerUI.gameObject.SetActive(false );
+        BowlingPowerUI.gameObject.SetActive(false);
+        DefenceUI.gameObject.SetActive(false );
         data = null;
     }
 
